8a91ea9e829ccf2007f341b42e46ee191a08b96d
Josh Archer <jarcher@greenriver.edu>
2021-04-15 10:26:37 -0700

    Added some starter files for an example of Spring MVC

---
+++ src/main/java/edu/greenriver/sdev/cookingtips/controllers/BakingTipsController.java
@@ -0,0 +1,17 @@
+package edu.greenriver.sdev.cookingtips.controllers;
+
+public class BakingTipsController
+{
+    private String[] bakingTips = {
+        "Always Have the Correct Butter Consistency.",
+        "Room Temperature is KEY.",
+        "Read the Recipe Before Beginning.",
+        "Always Have Ingredients Prepped.",
+        "Learn How to Measure.",
+        "Weigh Your Ingredients.",
+        "Get an Oven Thermometer.",
+        "Keep Your Oven Door Closed."
+    };
+
+
+}
---
+++ src/main/java/edu/greenriver/sdev/cookingtips/controllers/GrillingTipsController.java
@@ -0,0 +1,16 @@
+package edu.greenriver.sdev.cookingtips.controllers;
+
+public class GrillingTipsController
+{
+    private String[] bakingTips = {
+        "Start with a clean grill.",
+        "Don’t move the food around.",
+        "Don’t squeeze or flatten meats.",
+        "Keep a spray bottle handy for flare-ups.",
+        "Buy a meat thermometer.",
+        "Avoid putting cold foods straight on the grill.",
+        "Undercook foods, just slightly.",
+        "Rest all meat!"
+    };
+
+}
